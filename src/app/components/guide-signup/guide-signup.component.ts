d4ee7bed4dbd996dd136bcecb4b327e14cbbc051
Mohamed Taher Othmen <mohamedtaher.othman@gmail.com>
2025-04-22 13:31:10 +0100

    auth components/api/backend done

--- src/app/components/guide-signup/guide-signup.component.ts
+++ src/app/components/guide-signup/guide-signup.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Route, Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-
-//backend
-//import { HttpClient } from '@angular/common/http';
-
+import { MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 
 
 @Component({
   selector: 'app-guide-signup',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    FormsModule,
+    MatSnackBarModule
+  ],
   templateUrl: './guide-signup.component.html',
   styleUrls: ['./guide-signup.component.css']
 })
@@ -33,6 +37,56 @@ export class GuideSignupComponent {
     'Spanish', 'Russian', 'Chinese', 'Japanese', 'Korean',
     'Other'
   ];
+
+  first_name: string = '';
+  last_name: string = '';
+  email: string = '';
+  phone_number: string = '';
+  city: string = '';
+  language: string = '';
+  bio: string = '';
+  experience_years: string = '';
+  profile_picture_url: string = '';
+  password: string = '';
+
+  constructor(
+    private authSer: AuthService,
+    private route: Router,
+    private snackBar: MatSnackBar
+  ){}
+
+  onSubmit(){
+    this.authSer.signup({
+      first_name: this.first_name,
+      last_name: this.last_name,
+      email: this.email,
+      phone_number: this.phone_number,
+      city: this.city,
+      language: this.language,
+      bio: this.bio,
+      experience_years: this.experience_years,
+      profile_picture_url: this.profile_picture_url,
+      password: this.password
+    }).subscribe({
+      next: response => {
+        if (response.success){  
+          console.log(response.message);
+          this.snackBar.open('Account Created Succesfully', 'close', {duration:3000});
+          this.route.navigate(['/login']);
+        }else{
+          console.log(response.message);
+          this.snackBar.open('Error While Creating Account', 'close', {duration:3000});
+          this.route.navigate(['/signup/guide']);
+        }
+      },
+      error : error =>{
+        console.log(error.message);
+        this.snackBar.open('Error Occured while Creating Account', 'close', {duration:3000});
+        this.route.navigate(['/signup/guide']);
+      }
+    })
+  }
+
 }
 
   
